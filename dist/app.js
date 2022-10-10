223db228838edd949a5d2555af09eef5f21d85da
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-10 21:22:36 +0200

    code added to app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,6 @@
-console.log(123);
+const button = document.querySelector('button');
+const categoryList = document.querySelector('.category');
+//--------------------
+const calculatePrice = (originalPrice, hasDicount) => hasDicount ? originalPrice * 0.8 : originalPrice;
+button.addEventListener('click', () => {
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log(123)
+
+const button:HTMLButtonElement = document.querySelector('button');
+const categoryList:HTMLUListElement = document.querySelector('.category');
+//--------------------
+
+const calculatePrice = (originalPrice: number, hasDicount: boolean): number => hasDicount ? originalPrice * 0.8 : originalPrice
+
+
+button.addEventListener('click', () => {
+    
+})

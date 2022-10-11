c467600a241435bebed48aeaf57af91845839a61
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-11 17:57:41 +0200

    code added to app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('button');
 const categoryList = document.querySelector('.category');
 //--------------------
-const calculatePrice = (originalPrice, hasDicount) => hasDicount ? originalPrice * 0.8 : originalPrice;
+const calculatePrice = (originalPrice, hasDiscount) => hasDiscount ? originalPrice * 0.8 : originalPrice;
 button.addEventListener('click', () => {
+    const hasDiscount = Math.random() > 0.5 ? true : false;
+    const price = +document.querySelector('input').value;
+    console.log(`you have to pay ${calculatePrice(price, hasDiscount)}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ const button:HTMLButtonElement = document.querySelector('button');
 const categoryList:HTMLUListElement = document.querySelector('.category');
 //--------------------
 
-const calculatePrice = (originalPrice: number, hasDicount: boolean): number => hasDicount ? originalPrice * 0.8 : originalPrice
+const calculatePrice = (originalPrice: number, hasDiscount: boolean): number => hasDiscount ? originalPrice * 0.8 : originalPrice
 
 
 button.addEventListener('click', () => {
-    
+    const hasDiscount = Math.random()  > 0.5 ? true : false;
+    const price = +document.querySelector('input').value
+    console.log(`you have to pay ${calculatePrice(price, hasDiscount)}`);
+
 })
